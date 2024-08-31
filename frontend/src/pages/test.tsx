93cf5f1e8e3618e12ae87afa9ee77d4449c90b9e
chrisma71 <chris.pengfei.ma@gmail.com>
2024-08-30 20:50:05 -0400

    testpage

---
+++ frontend/src/pages/test.tsx
@@ -0,0 +1,12 @@
+// src/pages/TestPage.tsx
+import React from 'react';
+
+const TestPage: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-gray-800">caden chen is a pookie</h1>
+    </div>
+  );
+};
+
+export default TestPage;
