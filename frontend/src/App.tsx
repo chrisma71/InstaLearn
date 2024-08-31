8f305fa7cc6d6b14843b3d872c6db07094780ec0
chrisma71 <chris.pengfei.ma@gmail.com>
2024-08-31 02:44:35 -0400

    sign up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 // src/App.tsx
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
+import SignUpPage from './pages/Sign Up/SignUp';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </Router>
   );
 }
 
