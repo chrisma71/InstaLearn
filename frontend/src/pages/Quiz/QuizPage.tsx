3650e5a7bd6698fba92fceffd8d7a507719dd8d5
caden c <cadenc.woss@gmail.com>
2024-09-01 10:52:56 -0400

    add quiz

--- frontend/src/pages/Quiz/QuizPage.tsx
+++ frontend/src/pages/Quiz/QuizPage.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProgressionTree from './components/ProgressionTree';
+import Quiz from './components/Quiz';
 
 const QuizPage: React.FC = () => {
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+
+  const handleAnswerSelect = (answer: string) => {
+    setSelectedAnswer(answer);
+    // You can also handle what happens after selecting an answer here
+  };
+
+  const quizQuestion = "What is the derivative of x^2?";
+  const quizOptions = [
+    "2x",
+    "x",
+    "x^2",
+    "2"
+  ];
+
   return (
     <div className="flex flex-row">
-      {/* Progression Tree on the left */}
       <div className="w-1/2">
         <ProgressionTree />
       </div>
-
-      {/* Quiz section on the right */}
       <div className="w-1/2 p-4">
-        {/* Your quiz UI goes here */}
         <h2>Quiz Section</h2>
-        <p>This is where the quiz questions will be displayed.</p>
+        <Quiz 
+          question={quizQuestion} 
+          options={quizOptions} 
+          onAnswerSelect={handleAnswerSelect} 
+        />
+        {selectedAnswer && (
+          <p className="mt-4">
+            You selected: <strong>{selectedAnswer}</strong>
+          </p>
+        )}
       </div>
     </div>
   );
